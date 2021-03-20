00a2e3231923574e66b5ba19cbed99c3316e8886
startverywell <startverywell@gmail.com>
2021-03-20 18:24:49 +1100

    Updated manual tests

--- manual-tests/insertData.js
+++ manual-tests/insertData.js
@@ -1,4 +1,4 @@
-const o = {
+const jan012020 = {
   'original-values': [
     '2021-01-01 00:00:00 UTC',
 
@@ -33,13 +33,17 @@ const o = {
     '2021-01-01 00:00:00.123 UTC',
   ],
 };
+
+const data = [jan012020];
 const query = document.querySelector.bind(document);
 const table = query('table');
 
-for (let index = 0; index < o['original-values'].length; index++) {
-  const tr = document.createElement('tr');
-  const td = document.createElement('td');
-  td.innerHTML = o['original-values'][index];
-  tr.appendChild(td);
-  table.appendChild(tr);
+for (let indexOuter = 0; indexOuter < data.length; indexOuter++) {
+  for (let index = 0; index < data[indexOuter]['original-values'].length; index++) {
+    const tr = document.createElement('tr');
+    const td = document.createElement('td');
+    td.innerHTML = jan012020['original-values'][index];
+    tr.appendChild(td);
+    table.appendChild(tr);
+  }
 }
