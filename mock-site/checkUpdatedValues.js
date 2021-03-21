9a363ebaaae60f970a445f6f14564cb255d5935c
startverywell <startverywell@gmail.com>
2021-03-21 18:03:17 +1100

    Added popup

--- mock-site/checkUpdatedValues.js
+++ mock-site/checkUpdatedValues.js
@@ -4,8 +4,14 @@ const checks = '2021-01-01 19:00:00 AEDT';
 
 for (let index = 0; index < allElements.length; index++) {
   if (allElements[index].innerHTML == checks) {
-    allElements[index].insertAdjacentHTML('afterend', `<div class="success">✅</div>`);
+    allElements[index].insertAdjacentHTML(
+      'afterend',
+      `<div class="success">✅</div>`
+    );
   } else {
-    allElements[index].insertAdjacentHTML('afterend', `<div class="failed">❌</div>`);
+    allElements[index].insertAdjacentHTML(
+      'afterend',
+      `<div class="failed">❌</div>`
+    );
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,7 +1,46 @@
 (function () {
-  const runPageConversion = require('./run-conversion').default;
+  const query = document.querySelector.bind(document);
 
-  const ptTimeZone = 'America/Los_Angeles';
+  // Import conversion function
+  const runPageConversion = require('./run-page-conversion').default;
+
+  // Import popup button HTML, CSS and JS
+  const {
+    tzcPopupButton,
+    tzcPopupCSS,
+    toggleTZConverterPopup,
+    testconversion,
+  } = require('./popup/page-pop-up');
+
+  // Add CSS
+  const head = query('head');
+  const css = document.createElement('style');
+  css.innerHTML = tzcPopupCSS;
+
+  head.appendChild(css);
+
+  // Add popup button
+  const referenceNode = query('body');
+  const converterTool = document.createElement('div');
+  converterTool.innerHTML = tzcPopupButton;
+
+  referenceNode.appendChild(converterTool);
+
+  query('#tzc-open-button').onclick = toggleTZConverterPopup;
+  query('#tzc-close-options-button').onclick = toggleTZConverterPopup;
+
+  // Add listener for the picker to immediately run the conversion when new TimeZone is selected
+  const selectTimeZone = query('#tzc-full-list');
+
+  selectTimeZone.addEventListener('change', (event) => {
+    runPageConversion(
+      document.body.querySelectorAll('td'),
+      currentPageTimeZone,
+      event.target.value
+    );
+  });
+
+  // Run conversion
   const currentPageTimeZone = 'America/Los_Angeles';
   const toTimeZone = 'Australia/Canberra';
 
