c234641c17947787296710e0ae2739e0197b8d0f
startverywell <startverywell@gmail.com>
2021-04-14 17:53:39 +1000

    Added test data

---
+++ tests/mock-site/insertData.js
@@ -0,0 +1,52 @@
+const jan012020 = {
+  'original-values': [
+    '2021-01-01 00:00:00.123 UTC some other data',
+
+    '2018-05-22 13:16:26 PDT 1234567891',
+    '2021-01-01 00:00:00 UTC',
+
+    '2021-01-01 00:00:00 AEDT',
+
+    '2021-01-01 00:00:00 +12',
+
+    '2021-01-01 00:00:00 -12',
+
+    '2021-01-01 00:00:00 +1230',
+
+    '2021-01-01 00:00:00 -1230',
+
+    '2021-01-01 00:00:00',
+
+    '2021-01-01 00:00 UTC',
+
+    '2021-01-01 00:00 AEDT',
+
+    '2021-01-01 00:00 +12',
+
+    '2021-01-01 00:00 -12',
+
+    '2021-01-01 00:00 +1230',
+
+    '2021-01-01 00:00 -1230',
+
+    '2021-01-01 00:00',
+
+    '2021-01-01 00:00.123 UTC',
+
+    '2021-01-01 00:00:00.123 UTC',
+  ],
+};
+
+const data = [jan012020];
+const query = document.querySelector.bind(document);
+const table = query('table');
+
+for (let indexOuter = 0; indexOuter < data.length; indexOuter++) {
+  for (let index = 0; index < data[indexOuter]['original-values'].length; index++) {
+    const tr = document.createElement('tr');
+    const td = document.createElement('td');
+    td.innerHTML = jan012020['original-values'][index];
+    tr.appendChild(td);
+    table.appendChild(tr);
+  }
+}
